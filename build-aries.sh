ab0d6abed6e10f3fd1d6d5cc64044dce1a5ab3b0
DerRomtester <dieb.stefan.96@gmail.com>
2014-03-01 14:12:34 +0100

    neo name

--- build-aries.sh
+++ build-aries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Making KK-4.4/CM-11.0 kernel for GS1 i9000"
-BUILDVERSION=x-aries-cm-11.0-`date +%Y%m%d`-mtwrp-cma
+BUILDVERSION=Neo-cm-11.0-`date +%Y%m%d`-mtwrp-cma
 DATE_START=$(date +"%s")
 
 make "cyanogenmod_galaxysmtd_defconfig"
